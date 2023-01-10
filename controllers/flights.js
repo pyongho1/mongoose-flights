5d84599a048671261b1bc9b8f854a76de48a1f83
Yong Park <pyongho0306@gmail.com>
2023-01-10 00:11:41 -0800

    Add index

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,7 +1,34 @@
+import { Flight } from "../models/movie.js";
+
+function index(req, res) {
+  Flight.find({})
+    .then((flights) => {
+      res.render("flights/index", {
+        flights,
+        title: "All Flights",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
+}
+
 function newFlight(req, res) {
   res.render("flights/new", {
     title: "Add Flight",
   });
 }
 
-export { newFlight as new };
+function create(req, res) {
+  Flight.create(req.body)
+    .then((flight) => {
+      res.redirect("/flights/new");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
+}
+
+export { newFlight as new, create, index };
--- routes/flights.js
+++ routes/flights.js
@@ -3,6 +3,10 @@ import * as flightsCtrl from "../controllers/flights.js";
 
 const router = Router();
 
+router.get("/", flightsCtrl.index);
+
 router.get("/new", flightsCtrl.new);
+
+router.post("/", flightsCtrl.create);
 
 export { router };
